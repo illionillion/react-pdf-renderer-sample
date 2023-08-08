ca3a190f13a4e470e258e5ff7cbcc519008ef322
yuto ichinose <yuto.0526.sano@gmail.com>
2023-08-09 08:35:01 +0900

    複数ページ実装

--- src/components/PDFScreen.tsx
+++ src/components/PDFScreen.tsx
@@ -1,26 +1,70 @@
 import { Document, PDFViewer, Page, StyleSheet, Text, View } from "@react-pdf/renderer";
 import type { FC } from "react";
 
-interface documentDataType {
+interface PageType {
     title: string
+    contents: Content[]
+}
+
+interface Content {
+    heading: string,
     body: string
 }
 
 /**
  * データ
  */
-const documentData: documentDataType[] = [
+const documentData: PageType[] = [
     {
         title: "Title1",
-        body: "Content"
+        contents: [
+            {
+                heading: "Section1",
+                body: "Content"
+            },
+            {
+                heading: "Section2",
+                body: "Content"
+            },
+            {
+                heading: "Section3",
+                body: "Content"
+            },
+        ]
     },
     {
         title: "Title2",
-        body: "Content"
+        contents: [
+            {
+                heading: "Section1",
+                body: "Content"
+            },
+            {
+                heading: "Section2",
+                body: "Content"
+            },
+            {
+                heading: "Section3",
+                body: "Content"
+            },
+        ]
     },
     {
         title: "Title3",
-        body: "Content"
+        contents: [
+            {
+                heading: "Section1",
+                body: "Content"
+            },
+            {
+                heading: "Section2",
+                body: "Content"
+            },
+            {
+                heading: "Section3",
+                body: "Content"
+            },
+        ]
     },
 ]
 
@@ -32,12 +76,19 @@ const styles = StyleSheet.create({
         // flexDirection: 'row',
         backgroundColor: '#E4E4E4'
     },
-    section: {
+    content: {
         margin: 10,
         padding: 10,
         // flexGrow: 1
     },
+    section: {
+        paddingVertical: 10,
+    },
     title: {
+        fontSize: 22,
+        paddingVertical: 8,
+    },
+    heading: {
         fontSize: 20,
         paddingVertical: 5,
     },
@@ -52,14 +103,19 @@ const styles = StyleSheet.create({
  */
 const MyDocument: FC = () => (
     <Document>
-        <Page size="A4" style={styles.page}>
-            {documentData.map((item, index) => (
-                <View key={index} style={styles.section}>
+        {documentData.map((item, index) => (
+            <Page key={index} size="A4" style={styles.page}>
+                <View style={styles.content}>
                     <Text style={styles.title}>{item.title}</Text>
-                    <Text style={styles.body}>{item.body}</Text>
+                    {item.contents.map((content, i) => (
+                        <View key={i} style={styles.section}>
+                            <Text style={styles.heading}>{content.heading}</Text>
+                            <Text style={styles.body}>{content.body}</Text>
+                        </View>
+                    ))}
                 </View>
-            ))}
-        </Page>
+            </Page>
+        ))}
     </Document>
 );
 
